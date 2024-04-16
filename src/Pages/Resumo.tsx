e8f7b009ed1632d23c001159a614ce7be7cc3654
Dario de souza <darioo_dario@hotmail.com>
2024-04-15 21:24:37 -0300

    página resumo

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { useData } from "../Context/DataContext";
 
 const Resumo = () => {
-  return <div>Resumo</div>;
+  const { data } = useData()
+  if (data === null) return null
+  return <section>
+    <div className="resumo flex mb">
+      <div className="box">
+        <h2>Vendas</h2>
+        <span>{data.filter(venda => venda.status !== 'falha')
+          .reduce((acc, item) => acc + item.preco, 0)
+          .toLocaleString('pt-BR',
+            {
+              style: 'currency',
+              currency: 'BRL'
+            }
+          )}</span>
+      </div>
+      <div className="box">
+        <h2>Recebido</h2>
+        <span>{data.filter(i => i.status === 'pago')
+          .reduce((acc, item) => acc + item.preco, 0)
+          .toLocaleString('pt-BR',
+            {
+              style: 'currency',
+              currency: 'BRL'
+            }
+          )}</span>
+      </div>
+
+      <div className="box">
+        <h2>Processando</h2>
+        <span>{data.filter(i => i.status === 'processando')
+          .reduce((acc, item) => acc + item.preco, 0)
+          .toLocaleString('pt-BR',
+            {
+              style: 'currency',
+              currency: 'BRL'
+            }
+          )}</span>
+      </div>
+    </div>
+    <div className="box">Gráficos</div>
+  </section>;
 };
 
 export default Resumo;
